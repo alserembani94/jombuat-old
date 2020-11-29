9a254d977415b3767f5dc6266a61c1703f401993
Atif Aiman <alserembani94@gmail.com>
2020-11-29 21:13:53 +0800

    🩹 Fixing more CSS issue

--- postcss.config.js
+++ postcss.config.js
@@ -20,7 +20,6 @@ const purgecss = [
 module.exports = {
     plugins: [
         'tailwindcss',
-        process.env.NODE_ENV === 'production' ? purgecss : undefined,
         'postcss-preset-env',
     ],
 }
