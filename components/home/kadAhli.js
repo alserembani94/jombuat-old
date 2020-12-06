6020165abef4358e0f9b1cbd05784c5e9a1115f9
Atif Aiman <alserembani94@gmail.com>
2020-12-06 23:18:44 +0800

    📝 Tukar paparan hasil

--- components/home/kadAhli.js
+++ components/home/kadAhli.js
@@ -1,18 +1,37 @@
 import stail from '../../styles/Halaman.module.scss'
+import Image from 'next/image'
 
 const KadAhli = ({ senaraiKomuniti = [] }) => (
     <ul className={stail.result}>
         {
             senaraiKomuniti.map(ahli => (
-                <a href={ahli.pautan} target="_blank" rel="noreferrer noopener" key={ahli.row_id}>
+                // <a href={ahli.pautan} target="_blank" rel="noreferrer noopener" key={ahli.row_id}>
+                //     <li className={stail.user_card}>
+                //         <img className={stail.user_avatar} src={ahli.avatar} alt={ahli.nama} />
+                //         <p className={stail.user_name}>{ahli.nama}</p>
+                //         <p className={stail.user_designation}>{ahli.jawatan}</p>
+                //         <p className={stail.user_organisation}>{ahli.organisasi}</p>
+                //         <p className={stail.user_location}>{ahli.lokasi}</p>
+                //     </li>
+                // </a>
+                ahli.sorok !== 'TRUE' && 
                     <li className={stail.user_card}>
-                        <img className={stail.user_avatar} src={ahli.avatar} alt={ahli.nama} />
-                        <p className={stail.user_name}>{ahli.nama}</p>
-                        <p className={stail.user_designation}>{ahli.jawatan}</p>
-                        <p className={stail.user_organisation}>{ahli.organisasi}</p>
-                        <p className={stail.user_location}>{ahli.lokasi}</p>
+                        <a href={ahli.pautan} target="_blank" rel="noreferrer noopener" key={ahli.row_id}>
+                            <div className={stail.user_avatar}>
+                                <Image
+                                    src={ahli.avatar}
+                                    alt={ahli.nama}
+                                    layout="fill"
+                                    objectFit="cover"
+                                    objectPosition="center"
+                                />
+                            </div>
+                            <p className={stail.user_name}>{ahli.nama}</p>
+                            <p className={stail.user_designation}>{ahli.jawatan}</p>
+                            <p className={stail.user_organisation}>{ahli.organisasi}</p>
+                        </a>
+                        <button className={stail.user_hubung} data-visible={ahli.media_sosial !== ""}>Ikut</button>
                     </li>
-                </a>
             ))
         }
     </ul>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 module.exports = {
     images: {
         domains: [
-            'i.ibb.co'
+            'i.ibb.co',
+            'scontent.fkul8-1.fna.fbcdn.net',
+            'lh3.googleusercontent.com',
+            'pbs.twimg.com'
         ],
     },
 }
